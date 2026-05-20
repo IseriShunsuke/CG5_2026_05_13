4ecf7910bef13cd641f792cf6fce1bc6e38f16c9
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-05-20 11:34:10 +0900

    CG5_00_01

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -6,6 +6,9 @@ using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"LE3D_03_イセリ_シュンスケ");
+
 	DirectXCommon* dxcommon = DirectXCommon::GetInstance();
 
 	int w = dxcommon->GetBackBufferWidth();
@@ -53,11 +56,112 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	ID3DBlob* vsBlob = nullptr;
 	ID3DBlob* psBlob = nullptr;
-	ID3DBlob* errorBlob = nullptr;
 
 	std::wstring vsFile = L"Resources/shaders/testVs.hlsl";
 	hr = D3DCompileFromFile(vsFile.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", "vs_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &vsBlob, &errorBlob);
+	if (FAILED(hr))
+	{
+		DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
+		if (errorBlob)
+		{
+			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+
+	std::wstring psFile = L"Resources/shaders/testPS.hlsl";
+	hr = D3DCompileFromFile(psFile.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", "ps_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &psBlob, &errorBlob);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
+		if (errorBlob) {
+			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipeLineStateDesc{};
+	graphicsPipeLineStateDesc.pRootSignature = rootSignature;
+	graphicsPipeLineStateDesc.InputLayout = inputLayoutDesc;
+	graphicsPipeLineStateDesc.VS = {vsBlob->GetBufferPointer(), vsBlob->GetBufferSize()};
+	graphicsPipeLineStateDesc.PS = {psBlob->GetBufferPointer(), psBlob->GetBufferSize()};
+	graphicsPipeLineStateDesc.BlendState = blendDesc;
+	graphicsPipeLineStateDesc.RasterizerState = rasterizerDesc;
+
+	graphicsPipeLineStateDesc.NumRenderTargets = 1;
+	graphicsPipeLineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+
+	graphicsPipeLineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+	graphicsPipeLineStateDesc.SampleDesc.Count = 1;
+	graphicsPipeLineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+
+	ID3D12PipelineState* graphicsPipeLineState = nullptr;
+	hr = dxcommon->GetDevice()->CreateGraphicsPipelineState(&graphicsPipeLineStateDesc, IID_PPV_ARGS(&graphicsPipeLineState));
+	assert(SUCCEEDED(hr));
+
 	
+	D3D12_HEAP_PROPERTIES uploadHeapProiperties{};
+	uploadHeapProiperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	D3D12_RESOURCE_DESC vertexResourceDesc{};
+	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	vertexResourceDesc.Width = sizeof(Vector4) * 3;
+
+	vertexResourceDesc.Height = 1;
+	vertexResourceDesc.DepthOrArraySize = 1;
+	vertexResourceDesc.MipLevels = 1;
+	vertexResourceDesc.SampleDesc.Count = 1;
+
+	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	ID3D12Resource* vertexResource{};
+	hr = dxcommon->GetDevice()->CreateCommittedResource(&uploadHeapProiperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+	assert(SUCCEEDED(hr));
+
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
+	vertexBufferView.StrideInBytes = sizeof(Vector4);
+
+	Vector4* vertexData = nullptr;
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f};
+	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};
+	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};
+
+	vertexResource->Unmap(0, nullptr);
+
+	while (true) {
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+
+		dxcommon->PreDraw();
+
+		commondList->SetGraphicsRootSignature(rootSignature);
+		commondList->SetPipelineState(graphicsPipeLineState);
+		commondList->IASetVertexBuffers(0, 1, &vertexBufferView);
+
+		commondList->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		commondList->DrawInstanced(3, 1, 0, 0);
+
+		dxcommon->PostDraw();
+	}
+
+	vertexResource->Release();
+	graphicsPipeLineState->Release();
+	signatureBlob->Release();
+	if (errorBlob)
+	{
+		errorBlob->Release();
+	}
+	rootSignature->Release();
+	vsBlob->Release();
+	psBlob->Release();
+
+	KamataEngine::Finalize();
 	
 	return 0;
 }
