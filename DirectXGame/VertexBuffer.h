5eb77c39032fbe352d5f7412564c5c488210f0f7
IseriShunsuke <k024g0036@g.neec.ac.jp>
2026-06-17 18:06:25 +0900

    ok

--- DirectXGame/VertexBuffer.h
+++ DirectXGame/VertexBuffer.h
@@ -8,4 +8,11 @@ public:
 
 	ID3D12Resource* Get();
 	D3D12_VERTEX_BUFFER_VIEW* GetView();
+
+	VertexBuffer();
+	~VertexBuffer();
+
+private:
+	ID3D12Resource* vertexBuffer_;
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
 };
